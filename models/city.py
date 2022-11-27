3843350f6291874bc7481ebb7d61f1df947bc395
jaredatandi <jaredatandi07@gmail.com>
2022-11-27 14:11:27 +0300

    Add city class

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""the city module
+"""
+import models
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """A state class
+    """
+    state_id = ''
+    name = ''
