a160ac99cc5aba16a560a0728eab068b518ff50f
jaredatandi <jaredatandi07@gmail.com>
2022-11-27 14:11:16 +0300

    Define class amenity

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""An amenity class
+"""
+import models
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """takes name as parameter
+    """
+    name = ""
